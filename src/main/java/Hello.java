4f9b9bbcaa036b29536a706a0dbaec03325ac2fe
wechanhe <1470325185@qq.com>
2018-11-12 22:31:57 +0800

    init

---
+++ src/main/java/Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+    public void test(){
+        System.out.println("hello");
+    }
+}
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,6 +1,9 @@
+import org.junit.Test;
+
 public class MyTest {
     @Test
     public void test1(){
-        
+        Hello hello = new Hello();
+        hello.test();
     }
 }
