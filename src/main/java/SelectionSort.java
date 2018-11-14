8f0f8151d5d2674705579ebf316f74e40e0a4e55
wechanhe <1470325185@qq.com>
2018-11-14 23:48:50 +0800

    add selection sort

---
+++ src/main/java/SelectionSort.java
@@ -0,0 +1,21 @@
+public class SelectionSort<T extends Comparable> {
+    public T[] selectionSort(T[] numbers){
+        for (int i=0;i < numbers.length-1;i++){
+            int j = i+1;
+            if (numbers[j].compareTo(numbers[i])<0) {
+                T tmp = numbers[j];
+                int k = i;
+                // 查找应该插入的位置
+                for (; k >= 0; k--)
+                    if (numbers[k].compareTo(numbers[j]) < 0)
+                        break;
+                // 所有元素向后移动
+                for (int l = j; l > k+1; l--) {
+                    numbers[l] = numbers[l - 1];
+                }
+                numbers[k+1]=tmp;
+            }
+        }
+        return numbers;
+    }
+}
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -7,4 +7,13 @@ public class MyTest {
         Integer[] numbers = {1,2,3,4,5,6,7,8};
         System.out.println(binarysearch.binarySearch(numbers, 0));
     }
+
+    @Test
+    public void test_SelectionSort(){
+        SelectionSort<Float> selectionSort = new SelectionSort<Float>();
+        Float[] numbers = {2.0f,1.2f,4.4f,5.3f,7.1f,8.3f,4.7f,0.5f,9.1f,3.4f};
+        numbers = selectionSort.selectionSort(numbers);
+        for (int i=0;i<numbers.length;i++)
+            System.out.print(numbers[i]+" ");
+    }
 }
