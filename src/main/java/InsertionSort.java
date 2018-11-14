516d5e2ba6f89c5f37f3ff522bf4edb9eadba4a3
wechanhe <1470325185@qq.com>
2018-11-15 00:02:35 +0800

    add insertion sort

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -1,21 +1,21 @@
 public class InsertionSort<T extends Comparable> {
-    public T[] insertionSort(T[] numbers){
-        for (int i=0;i < numbers.length-1;i++){
+    public T[] insertionSort(T[] arr){
+        for (int i=0;i < arr.length-1;i++){
             int j = i+1;
-            if (numbers[j].compareTo(numbers[i])<0) {
-                T tmp = numbers[j];
+            if (arr[j].compareTo(arr[i])<0) {
+                T tmp = arr[j];
                 int k = i;
                 // 查找应该插入的位置
                 for (; k >= 0; k--)
-                    if (numbers[k].compareTo(numbers[j]) < 0)
+                    if (arr[k].compareTo(arr[j]) < 0)
                         break;
                 // 所有元素向后移动
                 for (int l = j; l > k+1; l--) {
-                    numbers[l] = numbers[l - 1];
+                    arr[l] = arr[l - 1];
                 }
-                numbers[k+1]=tmp;
+                arr[k+1]=tmp;
             }
         }
-        return numbers;
+        return arr;
     }
 }
