4d04efab8db47995c5d051be53bd6431c85786c9
wechanhe <1470325185@qq.com>
2018-11-15 00:00:43 +0800

    add binary search

--- src/main/java/Binarysearch.java
+++ src/main/java/Binarysearch.java
@@ -1,11 +1,11 @@
 public class Binarysearch<T extends Comparable> {
-    public int binarySearch(T[] numbers, T target){
-        int low = 0, high = numbers.length-1, mid;
+    public int binarySearch(T[] arr, T target){
+        int low = 0, high = arr.length-1, mid;
         while (low <= high){
             mid = (high + low)/2;
-            if (target.compareTo(numbers[mid]) == 0)
+            if (target.compareTo(arr[mid]) == 0)
                 return mid;
-            else if (target.compareTo(numbers[mid]) < 0){
+            else if (target.compareTo(arr[mid]) < 0){
                 high = mid - 1;
             }else {
                 low = mid + 1;
