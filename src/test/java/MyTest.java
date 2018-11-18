5fc344244616d73da892a091962356889160ebab
wechanhe <1470325185@qq.com>
2018-11-18 09:13:09 +0800

    add unit test

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -25,4 +25,22 @@ public class MyTest {
         for (int i=0;i<arr.length;i++)
             System.out.print(arr[i]+" ");
     }
+
+    @Test
+    public void test_DivideAndConquer(){
+        DivideAndConquer dv = new DivideAndConquer();
+        int[] arr = {1,2,3,10,5,6,7,8,9};
+        System.out.println("sum of arr:"+dv.sum(arr,0));
+        System.out.println("max of arr:"+dv.maxNum(arr, 0, arr.length-1));
+    }
+
+    @Test
+    public void test_QuickSort(){
+        QuickSort quickSort = new QuickSort();
+        int[] arr = {2,5,6,8,1,3,0,6,4,2,3};
+        quickSort.quickSort(arr, 0 , arr.length-1);
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
